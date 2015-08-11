1d3631f7deb6ddf0facad7ec08117c4b3f59c023
UserXXX <anonym.nutzer@web.de>
2015-08-11 17:07:44 +0200

    Began designing command editor
    Began to design the command editor.

---
+++ Claw/UI/Controls/BlockEditor.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Claw.UI.Controls
+{
+    /// <summary>
+    /// Logic for BlockEditor.xaml
+    /// </summary>
+    public partial class BlockEditor : UserControl
+    {
+        /// <summary>
+        /// Dependencyproperty for the BlockEditor title.
+        /// </summary>
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(BlockEditor), new PropertyMetadata(""));
+
+        /// <summary>
+        /// Title of the BlockEditor.
+        /// </summary>
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public BlockEditor()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
